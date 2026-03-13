b15881f0184b81fa3a77068388cb957c3172dc71
rrufino71@gmail.com <rrufino71@gmail.com>
2026-03-13 14:25:04 -0300

    login error

--- src/config/database.js
+++ src/config/database.js
@@ -12,17 +12,26 @@ const mysql = require("mysql2/promise");
 // });
 
 //produccion 1
+// const pool = mysql.createPool({
+//    host: process.env.MYSQLHOST,
+//    user: process.env.MYSQLUSER,
+//    password: process.env.MYSQ_ROOT_PASSWORD,
+//    database: process.env.MYSQLDATABASE,
+//    port: process.env.MYSQLPORT,
+//    waitForConnections: true,
+//    connectionLimit: 10
+// });
+
+//produccion prueba
 const pool = mysql.createPool({
-   host: process.env.MYSQLHOST,
-   user: process.env.MYSQLUSER,
-   password: process.env.MYSQL_ROOT_PASSWORD,
-   database: process.env.MYSQLDATABASE,
-   port: process.env.MYSQLPORT,
+   host: "mysql.railway.internal",
+   user: "root",
+   password: "gpoJpBCxaOecsrufRhzjriveoYfzKSOv",
+   database: "railway",
+   port: 3306,
    waitForConnections: true,
    connectionLimit: 10
 });
-
-
 
 
 module.exports = pool;
