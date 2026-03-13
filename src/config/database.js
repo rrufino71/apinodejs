c9bc9b55270d8ccb7ae23d33e81be43f4f27d365
rrufino71@gmail.com <rrufino71@gmail.com>
2026-03-13 14:15:45 -0300

    root passw

--- src/config/database.js
+++ src/config/database.js
@@ -12,26 +12,17 @@ const mysql = require("mysql2/promise");
 // });
 
 //produccion 1
-// const pool = mysql.createPool({
-//    host: process.env.MYSQLHOST,
-//    user: process.env.MYSQLUSER,
-//    password: process.env.MYSQLPASSWORD,
-//    database: process.env.MYSQLDATABASE,
-//    port: process.env.MYSQLPORT,
-//    waitForConnections: true,
-//    connectionLimit: 10
-// });
-
-//produccion prueba
 const pool = mysql.createPool({
-   host: "mysql.railway.internal",
-   user: "root",
-   password: "gpoJpBCxaOecsrufRhzjriveoYfzKSOv",
-   database: "railway",
-   port: 3306,
+   host: process.env.MYSQLHOST,
+   user: process.env.MYSQLUSER,
+   password: process.env.MYSQL_ROOT_PASSWORD,
+   database: process.env.MYSQLDATABASE,
+   port: process.env.MYSQLPORT,
    waitForConnections: true,
    connectionLimit: 10
 });
+
+
 
 
 module.exports = pool;
