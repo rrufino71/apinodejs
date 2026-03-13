727b288aab1bb3ed69d64e23da878f05abc40478
rrufino71@gmail.com <rrufino71@gmail.com>
2026-03-13 13:53:40 -0300

    viendo base de datos

--- src/config/database.js
+++ src/config/database.js
@@ -12,12 +12,14 @@ const mysql = require("mysql2/promise");
 // });
 
 //produccion
- const pool = mysql.createPool({
+const pool = mysql.createPool({
    host: process.env.MYSQLHOST,
    user: process.env.MYSQLUSER,
    password: process.env.MYSQLPASSWORD,
    database: process.env.MYSQLDATABASE,
-   port: process.env.MYSQLPORT
+   port: process.env.MYSQLPORT,
+   waitForConnections: true,
+   connectionLimit: 10
 });
 
 
