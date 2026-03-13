689272bbf2fe09ee0019948bd015599001bff659
rrufino71@gmail.com <rrufino71@gmail.com>
2026-03-13 12:55:36 -0300

    prueba

--- src/config/database.js
+++ src/config/database.js
@@ -2,14 +2,14 @@ const mysql = require("mysql2/promise");
 
 
 //desarrollo
-//const pool = mysql.createPool({
+// const pool = mysql.createPool({
 //  host: process.env.DB_HOST,
 //  user: process.env.DB_USER,
 //  password: process.env.DB_PASS,
 //  database: process.env.DB_NAME,
 //  waitForConnections: true,
 //  connectionLimit: 10
-//});
+// });
 
 //produccion
 const pool = mysql.createPool({
