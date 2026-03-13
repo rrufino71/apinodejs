bd936af68444002755df3db7a0d952a0287c2af1
rrufino71@gmail.com <rrufino71@gmail.com>
2026-03-13 09:51:57 -0300

    preparado para produccion

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 
 const app = require("./src/app");
 
-app.listen(3000, () => {
-  console.log("API corriendo en puerto 3000");
+//app.listen(3000, () => {
+//  console.log("API corriendo en puerto 3000");
+//});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`API corriendo en puerto ${PORT}`);
 });
--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,22 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10
-});
 
+//desarrollo
+//const pool = mysql.createPool({
+//  host: process.env.DB_HOST,
+//  user: process.env.DB_USER,
+//  password: process.env.DB_PASS,
+//  database: process.env.DB_NAME,
+//  waitForConnections: true,
+//  connectionLimit: 10
+//});
+
+//produccion
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+});
 module.exports = pool;
