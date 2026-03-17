a0b9795fbc4d6eec2a5f6a7c987fb60661d5ebe1
rrufino71@gmail.com <rrufino71@gmail.com>
2026-03-17 12:12:53 -0300

    manual

--- src/config/database.js
+++ src/config/database.js
@@ -34,26 +34,26 @@ const mysql = require("mysql2/promise");
 //});
 
 // //produccion hostinger manual
-//  const pool = mysql.createPool({
-//     host: "localhost",
-//     user: "u325607018_root",
-//     password: "Inti12223906",
-//     database: "u325607018_apidb",
-//     port: 3306,
-//     waitForConnections: true,
-//     connectionLimit: 10
-//  });
-
-//produccion hostinger con variables de entorno
  const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    host: "localhost",
+    user: "u325607018_root",
+    password: "Inti12223906",
+    database: "u325607018_apidb",
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10
  });
+
+// //produccion hostinger con variables de entorno
+//  const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT,
+//     waitForConnections: true,
+//     connectionLimit: 10
+//  });
 
 
 module.exports = pool;
