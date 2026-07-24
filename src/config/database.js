b9cfe93b8a902524c9dea99717540a9159b88ec4
rrufino71@gmail.com <rrufino71@gmail.com>
2026-07-24 06:25:03 -0300

    api con mysql

--- src/config/database.js
+++ src/config/database.js
@@ -3,16 +3,40 @@ const mysql = require("mysql2/promise");
 const isProduction = process.env.NODE_ENV === "production";
 
 
-const pool = mysql.createPool({
-    host: isProduction ? process.env.DB_HOST : process.env.DB_HOST,
-    user: isProduction ? process.env.DB_USER : process.env.DB_USER,
-    password: isProduction ? process.env.DB_PASSWORD : process.env.DB_PASS,
-    database: isProduction ? process.env.DB_NAME : process.env.DB_NAME,
-    port: isProduction ? process.env.DB_PORT : 3306,
-    waitForConnections: true,
-    connectionLimit: 10
-});
+const sql = require("mssql");
 
+const config = {
+    server: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: parseInt(process.env.DB_PORT),
+    options: {
+        encrypt: false,               // true para Azure
+        trustServerCertificate: true  // SQL Server local
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    }
+};
+
+const pool = new sql.ConnectionPool(config);
+
+module.exports = pool;
+
+
+
+// const pool = mysql.createPool({
+//     host: isProduction ? process.env.DB_HOST : process.env.DB_HOST,
+//     user: isProduction ? process.env.DB_USER : process.env.DB_USER,
+//     password: isProduction ? process.env.DB_PASSWORD : process.env.DB_PASS,
+//     database: isProduction ? process.env.DB_NAME : process.env.DB_NAME,
+//     port: isProduction ? process.env.DB_PORT : 3306,
+//     waitForConnections: true,
+//     connectionLimit: 10
+// });
 
 
 //produccion prueba rialway
